36d46841ccfc111b656add2be410146273d307e6
Mungai Munene <mungai.munene@gmail.com>
2024-07-31 17:25:12 +0300

    updated lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    });
+}
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { addingEventListener };
 }
